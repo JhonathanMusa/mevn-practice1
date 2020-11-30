f8f5dc887c22b9b601080a7ee86e68039fb636d4
jhonathanmusa <jonathansm891@gmail.com>
2020-11-30 18:18:02 -0500

    first commit: project creation

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+// Rutas
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Middleware para Vue.js router modo history
+const history = require("connect-history-api-fallback");
+app.use(history());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
